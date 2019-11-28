0a258656793c06b8cf934f6d4ad76d49ff772792
tao-bug <897384405@qq.com>
2019-11-28 12:07:30 +0800

    订单－查询--修改

--- meiduo_mall/apps/meiduo_admin/views/orders.py
+++ meiduo_mall/apps/meiduo_admin/views/orders.py
@@ -1,10 +1,17 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from apps.orders.models import OrderInfo
 from apps.meiduo_admin.serializers.orders import OrderSerializer
 from apps.meiduo_admin.utils.meiduo_pagination import MeiduoPagination
 
 
-class OrderViewSet(ModelViewSet):
-    queryset = OrderInfo.objects.all()
+class OrderViewSet(ReadOnlyModelViewSet):
+    # queryset = OrderInfo.objects.all()
+    def get_queryset(self):
+        queryset = OrderInfo.objects
+        keyword = self.request.query_params.get('keyword')
+        if keyword:
+            queryset = queryset.filter(order_id__contains=keyword)
+        queryset = queryset.order_by('-create_time')
+        return queryset
     serializer_class = OrderSerializer
     pagination_class = MeiduoPagination

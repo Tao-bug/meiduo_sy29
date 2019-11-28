cbb7ff74ee3e177f652a6061e919123cf7dfd332
tao-bug <897384405@qq.com>
2019-11-28 18:23:46 +0800

    组-重写创建--密码需要加密

--- meiduo_mall/apps/meiduo_admin/serializers/admin.py
+++ meiduo_mall/apps/meiduo_admin/serializers/admin.py
@@ -6,4 +6,18 @@ from apps.users.models import User
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'mobile']
+        # fields = ['id', 'username', 'email', 'mobile']
+        fields = ['id', 'username', 'email', 'mobile', 'password', 'groups', 'user_permissions']
+        read_only_field = ['id']
+
+    # 重写创建--密码需要加密
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+
+        # 密码加密
+        instance.set_password(validated_data.get('password'))
+        # 设置管理员
+        instance.is_staff = True
+        instance.save()
+
+        return instance

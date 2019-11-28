6256e0ff571435160145ec563e41eb43e4429cc9
tao-bug <897384405@qq.com>
2019-11-28 18:06:17 +0800

    组-查询所有组，返回后供添加管理员时选择

--- meiduo_mall/apps/meiduo_admin/views/groups.py
+++ meiduo_mall/apps/meiduo_admin/views/groups.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.models import Group
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from apps.meiduo_admin.serializers.groups import GroupSerializer
 from apps.meiduo_admin.utils.meiduo_pagination import MeiduoPagination
 
@@ -9,3 +11,10 @@ class GroupViewSet(ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     pagination_class = MeiduoPagination
+
+    # 查询所有组，返回后供添加管理员时选择
+    @action(methods=['GET'], detail=False)
+    def simple(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
